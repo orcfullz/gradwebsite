b01b735b375d8aa2b1d69bded07f45d8ae8da092
vina-setiawaty <153541462+vina-setiawaty@users.noreply.github.com>
2024-02-13 08:57:47 +0800

    Add files via upload

---
+++ scripts/script.js
@@ -0,0 +1,27 @@
+console.log("javascript loaded")
+
+const delta = 20;
+let startX;
+let startY;
+
+let element = document.getElementById("arrow-down");
+let navMenu = document.getElementById("navigation-menu");
+
+document.querySelector("#arrow-down").addEventListener('click', function (e) {
+    navMenu.style.display = "flex";
+    alert("unlocked");
+    element.classList.add("move-down")
+})
+
+element.addEventListener('mousedown', function (event) {
+  startY = event.pageY;
+});
+
+element.addEventListener('mouseup', function (event) {
+  const diffY = Math.abs(event.pageY - startY);
+
+  if (diffY > delta) {
+    alert("unlocked");
+    navMenu.style.display = "flex";
+  }
+});
