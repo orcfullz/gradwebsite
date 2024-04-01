7eb91217564bd49501065375d1367e44d606ca90
vina-setiawaty <vina.vn071@gmail.com>
2024-04-01 10:31:36 +0800

    tweaked animation for welcome page

--- scripts/script.js
+++ scripts/script.js
@@ -61,12 +61,12 @@ window.onload = () => {
 });
 
   document.querySelector("#arrow-right").addEventListener('click', function (e) {
-    document.querySelector("#arrow-right-drag").style.marginLeft = 0;
+    document.querySelector("#arrow-right-drag").classList.add("move-right");
     setTimeout(function () {
       document.querySelector("#main-page").scrollIntoView({behavior: "smooth"}); //will redirect to your blog page (an ex: blog.html)
     }, 300); //will call the function after 2 secs.
     setTimeout(function () {
-      document.querySelector("#arrow-right-drag").style.marginLeft = "-260px";
+      document.querySelector("#arrow-right-drag").classList.remove("move-right");
     }, 350)
 })
 }
