3e1644cf44189ff557970bb4c5d4e81c7430d014
vina-setiawaty <vina.vn071@gmail.com>
2024-05-14 16:57:06 +0800

    fixed project page for desktop

--- scripts/page.js
+++ scripts/page.js
@@ -23,7 +23,7 @@ window.onload = () => {
     })
 
     document.querySelector(".designer-project-snippet").addEventListener("click", (e) => {
-        window.location.pathname = "./project.html"
+        window.location.href = window.location.origin + "/project.html" + (params.get("id") ? "?id=" + params.get("id") : "")
     })
 
     handlePicResize();
