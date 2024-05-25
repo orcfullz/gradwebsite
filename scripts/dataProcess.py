6a736f881cad5eaec31d36be7d39c36a072c4b55
vina-setiawaty <vina.vn071@gmail.com>
2024-05-25 16:13:46 +0800

    updated project and designer thumbnail

--- scripts/dataProcess.py
+++ scripts/dataProcess.py
@@ -1,24 +1,46 @@
 import json
 
-with open("dataTemp.json", "r") as file1:
+with open("processingJson.json", "r") as file1:
     data = json.load(file1)
 
-with open("piclink.json", "r") as file2:
-    links = json.load(file2)
+# with open("piclink.json", "r") as file2:
+#     links = json.load(file2)
 
-found = False    
+# found = False    
+
+# for row in data:
+#     orilink = (row["picture"]).split("/")
+#     for link in links:
+#         namelink = (link).split("/")
+#         if not found:
+#             if namelink[-1] == orilink[-1]:
+#                 row["desktopPic"] = link
+#                 found = True
+#     found = False
+downloadProjectThumbnail = []
+downloadstring1 = []
 
 for row in data:
-    orilink = (row["picture"]).split("/")
-    for link in links:
-        namelink = (link).split("/")
-        if not found:
-            if namelink[-1] == orilink[-1]:
-                row["desktopPic"] = link
-                found = True
-    found = False
-    
-print(data[:5])
+    row["designerThumbnail"] = "./assets/designerThumbnail/" + row["desktopImg"].split('/')[-1]
+    row["projectThumbnail"] = "./assets/projectThumbnail/" + row["projectImgDesktop"].split('/')[-1]
+    downloadProjectThumbnail.append(row["projectImgDesktop"] + "\n")
+    downloadstring1.append(row["projectImgDesktop"] + "\n")
+    downloadstring1.append(row["projectImgMobile"] + "\n")
+    for section in row["project-section"]:
+        for img in section["pictures"]:
+            downloadstring1.append(img["img_link"] + "\n")
 
-with open("data.json", "w") as file:
+    
+print(data[0])
+# print(type(data))
+# print(type(data[0]))
+
+with open("processingData2.json", "w") as file:
     json.dump(data, file, indent=4)
+
+with open("downloadProjectThumbnail.txt", "w") as file:
+    file.writelines(downloadProjectThumbnail)
+
+with open("downloadProjectImages.txt", "w") as file:
+    file.writelines(downloadstring1)
+
